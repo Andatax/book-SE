d452682e2fe40562f780104a2a2e1b6f7581f6d9
Abraham Mendez <140019816+Andatax@users.noreply.github.com>
2024-02-14 09:06:01 -0600

    Client working, loggin modal display with apolloprovider

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,19 +2,17 @@ import { gql } from "@apollo/client";
 
 export const QUERY_ME = gql`
 	query me {
-		me {
-			_id
-			username
-			email
-			bookCount
-			savedBooks {
-				bookId
-				authors
-				description
-				image
-				link
-				title
-			}
+		_id
+		username
+		email
+		bookCount
+		savedBooks {
+			bookId
+			authors
+			description
+			image
+			link
+			title
 		}
 	}
 `;
